61258fc35fb790aa0c218cb5af5a99bfc2d3d587
Sebastian Appler Olsson <sebastian.applerolsson@spinit.se>
2017-04-28 17:57:49 +0200

    Added uSync and ModelsBuilder

---
+++ StackCMS/Features/Home/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace StackCMS.Features.Home
+{
+    public class HomeController : RenderMvcController
+    {
+        public override ActionResult Index(RenderModel model)
+        {
+            //Do some stuff here, then return the base method
+            var homeModel = new HomeModel(model.Content)
+            {
+                Titel = "krumler"
+            };
+            
+            return base.CurrentTemplate(homeModel);
+        }
+    }
+}
---
+++ StackCMS/Features/Home/HomeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web.Models;
+
+namespace StackCMS.Features.Home
+{
+    public class HomeModel : PublishedContentModel
+    {
+        public string Titel { get; set; }
+
+        public HomeModel(IPublishedContent content) : base(content)
+        {
+        }
+    }
+}
