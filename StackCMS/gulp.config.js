8793a49c775a42f0e9a3581b1ca7cd501bba0464
Sebastian Appler Olsson <sebastian@spinit.se>
2017-05-02 18:00:51 +0200

    Added gulp with js-injection and half function css injection

---
+++ StackCMS/gulp.config.js
@@ -0,0 +1,44 @@
+﻿module.exports = function () {
+  var root = './';
+  var distDir = root + 'dist/';
+  var jsDistDir = distDir + 'js/';
+  var jsSrcDir = root + 'Features/';
+  var stylesDir = root + 'assets/styles/';
+  var cssBuildDir = stylesDir + 'css/';
+
+  var stylesDistDir = distDir + 'styles/';
+  var cssDistDir = stylesDistDir + 'css/';
+  var index = root + 'Views/Shared/_Layout.cshtml';
+  var indexRoot = root + 'Views/Shared/';
+  var bower = root + 'bower.json';
+  var npm = root + 'package.json';
+
+  var config = {
+    distDir: distDir,
+    jsDistDir: jsDistDir,
+    jsDist: jsDistDir + '*.js',
+    jsSrc: [jsSrcDir + '**/*.js'],
+
+    htmlSrc: jsSrcDir + '**/*.html',
+
+    scssSrcWatch: stylesDir + 'scss/**/*.scss',
+    lessSrcWatch: stylesDir + 'less/**/*.less',
+    cssBuildDir: cssBuildDir,
+    cssBuild: cssBuildDir + '*.css',
+    cssDistDir: cssDistDir,
+    cssDist: cssDistDir + '*.css',
+    stylesDistDir: stylesDistDir,
+    stylesDir: stylesDir,
+    styles: stylesDir + '**/*.*',
+    bower: bower,
+	npm: npm,
+    index: index,
+	indexRoot: indexRoot,
+    root: root,
+
+    semanticSiteWatch: root + 'assets/semantic/src/site/**/*.{overrides,variables}',
+    semanticThemeWatch: root + 'assets/semantic/src/themes/**/*.{overrides,variables}'
+  }
+
+  return config;
+};
