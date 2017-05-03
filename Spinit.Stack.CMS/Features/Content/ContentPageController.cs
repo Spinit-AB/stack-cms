46a85499433dee330657eb92bf3a52de39def18a
Sebastian Appler Olsson <sebastian.applerolsson@spinit.se>
2017-05-03 19:16:29 +0200

    Added sync for created content

---
+++ Spinit.Stack.CMS/Features/Content/ContentPageController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace Spinit.Stack.CMS.Features.Content
+{
+    public class ContentPageController : RenderMvcController
+    {
+        public override ActionResult Index(RenderModel model)
+        {
+            var contentModel = new ContentPageModel(model.Content)
+            {
+                Test = "Test mvc"
+            };
+        
+            return base.CurrentTemplate(contentModel);
+        }
+    }
+}
---
+++ Spinit.Stack.CMS/Features/Content/ContentPageModel.cs
@@ -0,0 +1,13 @@
+﻿using Umbraco.Core.Models;
+
+namespace Spinit.Stack.CMS.Features.Content
+{
+    public class ContentPageModel : GeneratedModels.ContentPage
+    {
+        public string Test { get; set; }
+
+        public ContentPageModel(IPublishedContent content) : base(content)
+        {
+        }
+    }
+}
