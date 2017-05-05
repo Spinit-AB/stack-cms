2d9e03e2b3a4a677a82aad15ca6055e287361fa4
Sebastian Appler Olsson <sebastian.applerolsson@spinit.se>
2017-05-05 15:13:54 +0200

    Added angular bootstrap

--- Spinit.Stack.CMS/Features/angular.module.js
+++ Spinit.Stack.CMS/Features/angular.module.js
@@ -1,7 +1,11 @@
 ﻿(function () {
     'use strict';
 
-    var angularApp = angular.module('Spinit.Stack.CMS',[]);
+    var angularApp = angular.module('Spinit.Stack.CMS',
+        [
+            'ui.bootstrap',
+            'ngAnimate'
+            ]);
 
     angularApp.config(function () {
 
