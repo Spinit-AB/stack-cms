02a1bcf06014c9b2e72812d8caad51e808609f68
Sebastian Appler Olsson <sebastian.applerolsson@spinit.se>
2017-07-14 14:26:37 +0200

    Fixed bug with javascript crash when not finding the HomePage div

--- Spinit.Stack.CMS/Features/Home/HomeIndex.tsx
+++ Spinit.Stack.CMS/Features/Home/HomeIndex.tsx
@@ -2,7 +2,11 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import HomePage from './HomePage';
 
-render(
-    <HomePage />,
-  document.getElementById('HomePage')
-);
+let element = document.getElementById('HomePage');
+
+if (element) {
+    render(
+        <HomePage />,
+        element
+    );
+}
