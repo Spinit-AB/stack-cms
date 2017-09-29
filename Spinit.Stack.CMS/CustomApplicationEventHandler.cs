0c7684c4a30fd27f9aa1c6015d92cd8c93b98731
Sebastian Appler Olsson <sebastian.applerolsson@spinit.se>
2017-09-29 16:29:56 +0200

    Return JSON from Umbraco Api Controller

---
+++ Spinit.Stack.CMS/CustomApplicationEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using Umbraco.Core;
+
+namespace Spinit.Stack.CMS
+{
+    public class CustomApplicationEventHandler : ApplicationEventHandler
+    {
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings
+                .Add(new RequestHeaderMapping("Accept",
+                    "text/html",
+                    StringComparison.InvariantCultureIgnoreCase,
+                    true,
+                    "application/json"));
+        }
+    }
+}
---
+++ Spinit.Stack.CMS/Features/ContentApi/ContentPageApiController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Spinit.Stack.CMS.Features.Content;
+using Umbraco.Web.Models;
+using Umbraco.Web.WebApi;
+
+namespace Spinit.Stack.CMS.Features.ContentApi
+{
+    public class ContentPageApiController : UmbracoApiController
+    {
+        [HttpGet]
+        public IEnumerable<string> Pages()
+        {
+            //var root = Umbraco.TypedContentAtRoot().First();
+        
+            return new []{ "test"};
+        }
+    }
+}
