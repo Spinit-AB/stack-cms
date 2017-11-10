72ef3106297d29a70b02b92842e84fb3821f4f81
Sebastian Appler Olsson <sebastian.applerolsson@spinit.se>
2017-11-10 09:33:45 +0100

    Return page by id as json

--- Spinit.Stack.CMS/Features/ContentApi/ContentPageApiController.cs
+++ Spinit.Stack.CMS/Features/ContentApi/ContentPageApiController.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web.Http;
+using System.Web.Mvc;
+using Newtonsoft.Json;
 using Spinit.Stack.CMS.Features.Content;
+using Umbraco.Core.Models;
+using Umbraco.Web;
 using Umbraco.Web.Models;
 using Umbraco.Web.WebApi;
 
@@ -9,12 +12,27 @@ namespace Spinit.Stack.CMS.Features.ContentApi
 {
     public class ContentPageApiController : UmbracoApiController
     {
-        [HttpGet]
-        public IEnumerable<string> Pages()
+        [System.Web.Http.HttpGet]
+        public object Page(int id)
         {
             //var root = Umbraco.TypedContentAtRoot().First();
-        
-            return new []{ "test"};
+
+            var page = Umbraco.TypedContent(id);
+
+            var customProperties = page.Properties.ToDictionary(prop => prop.PropertyTypeAlias, prop => prop.Value);
+
+            var umbracoProperties = new Dictionary<string, object>
+            {
+                {"Id", page.Id},
+                {"Url", page.Url},
+                {"Name", page.Name},
+                {"WriterName", page.WriterName},
+                {"CreateDate", page.CreateDate},
+                {"DocumentTypeAlias", page.DocumentTypeAlias},
+                {"CustomProperties", customProperties}
+            };
+
+            return umbracoProperties;
         }
     }
 }
