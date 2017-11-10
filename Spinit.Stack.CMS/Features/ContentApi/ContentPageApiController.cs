a5bdd4bcbbb5644f7532c1a9b6f82748b24d082d
Sebastian Appler Olsson <sebastian.applerolsson@spinit.se>
2017-11-10 14:11:56 +0100

    Installed Vorto and added support for getting page in diffrent lanugage

--- Spinit.Stack.CMS/Features/ContentApi/ContentPageApiController.cs
+++ Spinit.Stack.CMS/Features/ContentApi/ContentPageApiController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
+using Our.Umbraco.Vorto.Extensions;
+using umbraco.cms.presentation.create.controls;
 using Umbraco.Core;
 using Umbraco.Web.Extensions;
 using Umbraco.Web.WebApi;
@@ -9,27 +12,25 @@ namespace Spinit.Stack.CMS.Features.ContentApi
     public class ContentPageApiController : UmbracoApiController
     {
         [System.Web.Http.HttpGet]
-        public object Page(int id)
+        public object Page(int id, string language = null)
         {
             var page = Umbraco.TypedContent(id);
 
             var contentType = Services.ContentService.GetById(page.Id);
 
             var customProperties = contentType.Properties.ToDictionary(prop => prop.Alias,
-                property => new
-                {
-                    Value = 
+                property => 
                     property.PropertyType?.PropertyEditorAlias == "Umbraco.MediaPicker2" ?
                     Udi.Parse(property.Value.ToString()).ToPublishedContent().Url :
 
                     property.PropertyType?.PropertyEditorAlias == "Umbraco.ContentPicker2" ?
-                    Udi.Parse(property.Value.ToString()).ToPublishedContent().Id
+                    Udi.Parse(property.Value.ToString()).ToPublishedContent().Id :
 
-                    :  
+                    property.PropertyType?.PropertyEditorAlias == "Our.Umbraco.Vorto" ?
+                    page.GetVortoValue(property.Alias, language) :  
                     
-                    property.Value,
-                    Type = property.PropertyType?.PropertyEditorAlias,
-                });
+                    property.Value
+                );
 
             var umbracoProperties = new Dictionary<string, object>
             {
